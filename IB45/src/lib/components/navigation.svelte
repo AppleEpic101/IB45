23dcfaaa0e2fa53ea1c1d041a14d6f07e73c68cb
Alexander Du <du.alexander123@gmail.com>
2024-09-20 15:23:55 -0700

    good css

--- IB45/src/lib/components/navigation.svelte
+++ IB45/src/lib/components/navigation.svelte
@@ -3,18 +3,20 @@
 </script>
 
 <nav>
-	<a href="/">
-		<div class="logo-group">
-			<img id="img" src="/favicon.ico" alt="IB SCORE CALCULATOR" />
-			<div class="logo-text">IB Predict</div>
-		</div>
-	</a>
+	<div class="nav-group">
+		<a href="/">
+			<div class="logo-group">
+				<img id="img" src="/favicon.ico" alt="IB SCORE CALCULATOR" />
+				<div class="logo-text">IB Predict</div>
+			</div>
+		</a>
 
-	<div class="top-links">
-		<a href="/">Home</a>
-		<a href="/subjects">Subjects</a>
-		<a href="/changelog">Changelog</a>
-		<a href="/faq">FAQ</a>
+		<div class="top-links">
+			<a href="/">Home</a>
+			<a href="/subjects">Subjects</a>
+			<a href="/changelog">Changelog</a>
+			<a href="/faq">FAQ</a>
+		</div>
 	</div>
 </nav>
 
@@ -23,33 +25,38 @@
 	$font-family: 'Space Grotesk', sans-serif;
 
 	nav {
-		display: flex;
 		background-color: var(--primary);
 		justify-content: space-between;
 		border-bottom: 1px solid black;
-		.logo-group {
+		.nav-group {
 			display: flex;
-			flex-direction: row;
-			align-items: center;
+			width: 950px;
+			margin: 0 auto;
 
-			#img {
-				width: 50px;
-				margin: 5px 20px;
-				transition: 0.3s ease-in-out;
+			.logo-group {
+				display: flex;
+				flex-direction: row;
+				align-items: center;
 
-				&:hover {
-					transform: scale(1.05) rotate(360deg);
-					-webkit-transform: scale(1.05) rotate(360deg);
-					transition: transform 0.5s ease-in-out;
-					-webkit-transition: -webkit-transform 0.5s ease-in-out;
+				#img {
+					width: 50px;
+					margin: 5px 20px;
+					transition: 0.3s ease-in-out;
+
+					&:hover {
+						transform: scale(1.05) rotate(360deg);
+						-webkit-transform: scale(1.05) rotate(360deg);
+						transition: transform 0.5s ease-in-out;
+						-webkit-transition: -webkit-transform 0.5s ease-in-out;
+					}
 				}
-			}
 
-			.logo-text {
-				font-family: $font-family;
-				font-size: 2em;
-				white-space: nowrap;
-				margin-right: 20px;
+				.logo-text {
+					font-family: $font-family;
+					font-size: 1.75em;
+					white-space: nowrap;
+					margin-right: 20px;
+				}
 			}
 		}
 
@@ -61,49 +68,46 @@
 			width: 100%;
 			font-family: $font-family;
 			a {
-				margin-right: 10px;
-				margin-left: 20px;
+				padding: 10px;
 				color: black;
 				text-decoration: none;
 				font-size: 1.2em;
+
+				&:hover {
+					background-color: var(--banner);
+					color: white;
+					transition: background-color 0.3s ease, color 0.3s ease;
+				}
 			}
 		}
 
-		@media screen and (max-width: 720px) {
+		@media screen and (max-width: 950px) {
+			.nav-group {
+				width: 100%;
+			}
+		}
+
+		@media screen and (max-width: 600px) {
 			.logo-text {
 				display: none;
 			}
 		}
 
-		@media screen and (max-width: 560px) {
-			.top-links a {
-				margin-right: 5px;
-				margin-left: 10px;
-				color: black;
-				text-decoration: none;
-				font-size: 0.8em;
-			}
-		}
-
-		@media screen and (max-width: 415px) {
-			img {
-				width: 50px;
-			}
-		}
-
-		@media screen and (max-width: 360px) {
+		@media screen and (max-width: 450px) {
 			img {
 				display: none;
 			}
-
-			height: 50px;
 
 			.top-links {
 				display: flex;
 				justify-content: space-evenly;
 				align-items: center;
-				margin-right: 5px;
 				width: 100%;
+
+				a {
+					margin: 10px 0;
+					font-size: 0.8em;
+				}
 			}
 		}
 	}
