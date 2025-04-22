86422b8e5200d425ba758f0b1c9be159486ff829
Alexander Du <du.alexander123@gmail.com>
2025-04-22 02:09:53 -0700

    footenote update

--- src/routes/subjects/[name]/+page.svelte
+++ src/routes/subjects/[name]/+page.svelte
@@ -355,13 +355,21 @@
 				<p class="p">*Timezone 0 (Worldwide)</p>
 				<p class="p">*Timezone 1 (North America, South America)</p>
 				<p class="p">*Timezone 2 (Europe, Africa, Asia, Australia, Oceania)</p>
+				<br />
 				<p class="p">
-					NOTE: N20, M21, N21, M22, N22 grade boundaries were artificially low due to COVID. There
-					were also
+					NOTE: N20, M21, N21, M22, N22 grade boundaries were artificially low due to COVID.
 				</p>
 				<p class="p">
 					NOTE: Beware of using outdated grade boundaries. Many subjects have since updated their
 					syllabus.
+				</p>
+				<p class="p">
+					NOTE: As a general trend, Timezone 2 usually have higher grade boundaries than Timezone 1.
+					Make sure you use the correct timezone.
+				</p>
+				<p class="p">
+					NOTE: As a general trend, November grade boundaries tend to be higher than those for May.
+					Be careful not to compare apples to oranges!
 				</p>
 			</div>
 		{/if}
